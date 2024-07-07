cb085f5dd6a3744c8f7aeaaa710699a62fcc2424
Scott Feichter <scottfeichter@gmail.com>
2024-07-07 13:50:52 -0700

    misc

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,22 @@
 // import { useState } from 'react'
+import { NavBar } from "./components/NavBar"
 
 function App() {
-  return <h1> Hello from App </h1>;
+const element = "H2";
+
+
+console.log("console.log", element);
+
+
+return(
+    <>
+    <h1>Hello from App</h1>
+    <h2></h2>
+    <h3>H3</h3>
+    <NavBar />
+    </>
+
+  )
 }
 
 export default App;
---
+++ src/components/NavBar/NavBar.jsx
@@ -0,0 +1,21 @@
+import { NavLink } from "react-router-dom";
+
+
+function NavBar() {
+
+     return(
+
+     <>
+     <nav>
+        <NavLink >
+            This is NavLink
+        </NavLink>
+        This is nav
+    </nav>
+
+     </>
+     )
+}
+
+
+export default NavBar;
